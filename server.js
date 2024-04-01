eb74459e65eaa1b4faa626a6dcfef56991161f53
csrgrr <guerramendezcesar@gmail.com>
2024-04-01 12:33:41 +0200

    a

--- server.js
+++ server.js
@@ -23,3 +23,47 @@ app.use('/model.json', falcorExpress.dataSourceRoute(function (req, res) {
 // serve static files from current directory
 app.use(express.static(__dirname + '/'));
 var server = app.listen(3000, () => console.log('Server on port 3000'));
+
+//--------------------------------------------------------
+// CASO DE CONEXIÓN A BD REAL CON MONGODB
+//--------------------------------------------------------
+
+// const mongoose = require('mongoose');
+
+// // Conexión a la base de datos MongoDB
+// mongoose.connect('mongodb://localhost:27017/falcor_movies', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// // Definición del esquema de la película
+// const movieSchema = new mongoose.Schema({
+//   title: String,
+//   year: Number
+// });
+
+// // Modelo de película
+// const Movie = mongoose.model('Movie', movieSchema);
+
+// // Configuración de la ruta Falcor
+// const MoviesRouter = require('falcor-router')([
+//   {
+//     route: 'moviesById[{integers:indices}]["title", "year"]',
+//     get: function(pathSet) {
+//       const indices = pathSet.indices;
+//       return Movie.find({ _id: { $in: indices } }).then(movies => {
+//         const results = [];
+//         movies.forEach(movie => {
+//           const index = indices.indexOf(movie._id);
+//           pathSet[2].forEach(property => {
+//             results.push({
+//               path: ['moviesById', index, property],
+//               value: movie[property]
+//             });
+//           });
+//         });
+//         return results;
+//       });
+//     }
+//   }
+// ]);
+
+// // Usar Falcor como middleware
+// app.use('/model.json', FalcorServer.dataSourceRoute(() => new MoviesRouter()));
